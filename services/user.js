81f3054ef9df835d74ecf91910cff2629255f05d
Mayur <mayuragarwal2812@gmail.com>
2021-04-11 14:46:59 +0530

    commit

--- services/user.js
+++ services/user.js
@@ -1,15 +1,23 @@
-// aconst validator = require("../utilities/validate");
 const model = require("../models/user");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
+const validator = require("../utilities/validate");
 require("dotenv").config();
 let userService = {};
 
-userService.LoginUser = async (Obj) => {
-  const getLogin = await model.LoginUser(Obj);
-  return jwt.sign(_.pick(getLogin, ["userid"]), process.env.TOKEN_SECRET);
+userService.LoginUser = async (obj) => {
+  if (validator.LoginUser(obj)) {
+    const getLogin = await model.LoginUser(obj);
+    return jwt.sign(_.pick(getLogin, ["userid"]), process.env.TOKEN_SECRET);
+  } else {
+    let err = new Error();
+    err.status = 400;
+    err.message =
+      "Invalid credentials,Please check the credentials and try again";
+    throw err;
+  }
 };
 userService.RegisterUser = async (Obj) => {
   Obj.userid = uuidv4();
@@ -24,14 +32,36 @@ userService.RegisterUser = async (Obj) => {
     userObj.password,
     await bcrypt.genSalt(10)
   );
-  const getResponse = await model.RegisterUser(userObj);
-  return jwt.sign(_.pick(getResponse, ["userid"]), process.env.TOKEN_SECRET);
+  if (validator.RegisterUser(userObj)) {
+    const getResponse = await model.RegisterUser(userObj);
+    return jwt.sign(_.pick(getResponse, ["userid"]), process.env.TOKEN_SECRET);
+  } else {
+    let err = new Error();
+    err.status = 400;
+    err.message =
+      "Invalid credentials,Please check the credentials and try again";
+    throw err;
+  }
 };
 userService.forgetPass = async (obj) => {
-  return await model.forgetPass(obj);
+  if (validator.forgetPass(obj.email)) return await model.forgetPass(obj);
+  else {
+    let err = new Error();
+    err.status = 400;
+    err.message =
+      "Invalid credentials,Please check the credentials and try again";
+    throw err;
+  }
 };
 userService.ResetUser = async (obj) => {
-  obj.password = await bcrypt.hash(obj.password, await bcrypt.genSalt(10));
-  return await model.ResetUser(obj);
+  if (validator.ResetUser(obj.password)) {
+    obj.password = await bcrypt.hash(obj.password, await bcrypt.genSalt(10));
+    return await model.ResetUser(obj);
+  } else {
+    let err = new Error();
+    err.status = 400;
+    err.message ="Invalid credentials,Please check the credentials and try again";
+    throw err;
+  }
 };
 module.exports = userService;
